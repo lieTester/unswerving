e55378cfa5dd0980942129c419debb11c800c261
puneet khandal <lietester.00@gmail.com>
2023-05-17 16:52:26 +0530

    change in vercel

--- build_files.sh
+++ build_files.sh
@@ -2,9 +2,9 @@ echo "BUILD START"
 python3.9 -m pip install -r requirements.txt 
 
 echo "make migrations"
-python3.9 -m manage.py makemigrations --noinput
-python3.9 -m manage.py migrate --noinput
+python3.9 -m python manage.py makemigrations --noinput
+python3.9 -m python manage.py migrate --noinput
 
 echo "collectstatic"
-python3.9 -m manage.py collectstatic --noinput --clear
+python3.9 -m python manage.py collectstatic --noinput --clear
 echo "BUILD END"
